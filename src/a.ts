1bef856dc30c05042438edf70b82940ad22be45f
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-06-12 03:30:49 +0530

    implementing Interfaces

--- src/a.ts
+++ src/a.ts
@@ -1,47 +1,23 @@
-// const user = {
-//     firstName: "prabhakar",
-//     lastName: "singh",
-//     email: "xyz@ymail.com",
-//     age:21
-// }
-
-// function isLegal(user: {         
-//     firstName: string,
-//     lastName: string,         // if we again want to the user value again we have to define all the types again 
-//     email: string,            // so we use iterface to get all this code gaian and again
-//     age: number
-// }): boolean{
-//     if(user.age>18){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-
-interface User{
-    firstName: string,
-    lastName: string,         
-    email: string,            
+interface Person {
+    name: string,
     age: number,
-    phone?: number,      // t is an optional either we pass it or not it not gives an error
+    greet(phrase: string): void;
 }
 
-const user = {
-    firstName: "prabhakar",
-    lastName: "singh",
-    email: "xyz@ymail.com",
-    age:21
-}
+class Employee implements Person {
+    name: string;
+    age: number;
 
-function isLegal(user: User): boolean{
-    if(user.age>18){
-        return true;
+    constructor(n:string, a:number){
+        this.name = n;
+        this.age = a;
     }
-    else{
-        return false;
+
+    greet(phrase: string): void {
+        console.log(`${phrase} ${this.name}`)
     }
 }
 
-const valuee = isLegal(user);
-console.log(valuee);
+const e = new Employee("Prabhakar", 22);
+// console.log(e.name); 
+e.greet("Hello there")
