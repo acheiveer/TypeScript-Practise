32b798bebec2999807ba07ea3f9c7ff7159059d3
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-06-12 02:10:49 +0530

    Type Inference

--- a.ts
+++ a.ts
@@ -1,5 +1,20 @@
- function greet(firstName: string){
-   console.log("Hello " + firstName)
+ function sum(a: number, b: number): number  //we donot need to add number expilicity function automatic understand it as return number due to Type inference
+ {     
+   return a+b;
  };
 
- greet("Prabhakar");
+const value = sum(1,2);
+console.log(value);
+
+function isLegal(age: number){
+    if(age>18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// const x = isLegal(18);
+const x: boolean = isLegal(18); // we donot need to add bollean expilicity x automatic understand it as boolean due to Type inference
+
