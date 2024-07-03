a22869661758d67259dc73c6af302b9e32fcf0d9
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-07-04 02:36:56 +0530

    partial in ts

--- src/a.ts
+++ src/a.ts
@@ -1,14 +1,18 @@
 interface User {
-  id: number;
+  id: string;
   name: string;
+  age: string;
   email: string;
-  createdAt: Date;
-}
-
-// For a profile display, only pick `name` and `email`
-type UserProfile = Pick<User, 'name' | 'email'>;
-
-const displayUserProfile = (user: UserProfile) => {
-  console.log(`Name: ${user.name}, Email: ${user.email}`);
+  password: string;
 };
-displayUserProfile({name:'prabhakar',email:'xyz@ymail.com'});
+
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+
+type UpdatePropsOptional = Partial<UpdateProps>
+
+function updateUser(updatedProps: UpdatePropsOptional) {
+  const {name,email}= updatedProps;
+  // hit the database tp update the user
+  console.log(`Name: ${name}, Email: ${email}`);
+}
+updateUser({name:'prabhakar',email:'xyz@ymail.com'})
