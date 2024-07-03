907ea95a830db842f6df4d41141c5c494a280c64
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-07-04 03:13:21 +0530

    type interference in zod

--- src/a.ts
+++ src/a.ts
@@ -1,11 +1,27 @@
-interface Config {
-  readonly endpoint: string;     // making the endpoint and apikey constant so that it cannot be changed further
-  readonly apiKey: string;
-}
+import { z } from 'zod';
+import express from "express";
 
-const config: Readonly<Config> = {
-  endpoint: 'https://api.example.com',
-  apiKey: 'abcdef123456',
-};
+const app = express();
 
-// config.apiKey = 'newkey'; // Error: Cannot assign to 'apiKey' because it is a read-only property.
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+type FinalUseschema = z.infer<typeof userProfileSchema>;
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody:FinalUseschema = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
